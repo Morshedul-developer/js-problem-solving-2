a23ca82b5e0aa3e33f1f8a37f63984b90355cf9e
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-03 12:29:26 +0600

    solved two js problems

---
+++ tasks.js
@@ -0,0 +1,35 @@
+//* Task -1:
+//? Find the lowest number in the array below.
+
+// const heights2 = [167, 190, 120, 165, 137];
+
+// function lowestNumber(numbers) {
+//     let min = numbers[0];
+//     for(const number of numbers) {
+//         if(number < min) {
+//             min = number;
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(lowestNumber(heights2));
+
+
+//* Task -2:
+//? Find the friend with the smallest name.
+
+// const heights2 = ['raju', 'robin', 'rafi', 'ron', 'hi', 'rashed'];
+
+// function smallestName(str) {
+//     let min = str[0];
+//     for(const char of str) {
+//         if(char.length < min.length) {
+//             min = char;
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(smallestName(heights2));
+
